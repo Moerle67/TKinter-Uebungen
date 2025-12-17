288c2f5d0fe49b44716d7b25a7fe9d9abf77c07b
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2025-12-17 12:36:20 +0100

    01

--- neu01.py
+++ neu01.py
@@ -1,3 +1,13 @@
 import tkinter as tk
 
-tk._test()
+root = tk.Tk()
+label = tk.Label(root, text="Hellow World!")
+but1 = tk.Button(root, text="Text ändern")
+but2 = tk.Button(root, text="Neues Fenster")
+but3 = tk.Button(root, text="Fenster schließen")
+but1.pack()
+but2.pack()
+but3.pack()
+label.pack()
+
+root.mainloop()
