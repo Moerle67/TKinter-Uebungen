3609dd4cdd49482a8cf7c3d2d93b4d31a016949c
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2025-12-18 09:29:01 +0100

    Buttons

--- neu01.py
+++ neu01.py
@@ -2,12 +2,25 @@ import tkinter as tk
 
 root = tk.Tk()
 label = tk.Label(root, text="Hellow World!")
-but1 = tk.Button(root, text="Text ändern")
-but2 = tk.Button(root, text="Neues Fenster")
-but3 = tk.Button(root, text="Fenster schließen")
+
+def text_aendern():
+    label.config(text="Bye")
+
+def neues_fenster():
+    root2 = tk.Tk()
+    label2 = tk.Label(root2, text="zweites Fenster")
+    label2.pack()
+    root2.mainloop()
+
+def fenster_schließen():
+    root.destroy()
+
+but1 = tk.Button(root, text="Text ändern", command=text_aendern)
+but2 = tk.Button(root, text="Neues Fenster", command=neues_fenster)
+but3 = tk.Button(root, text="Fenster schließen", command=fenster_schließen)
+label.pack()
 but1.pack()
 but2.pack()
 but3.pack()
-label.pack()
 
 root.mainloop()
